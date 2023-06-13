274aa85b30b3cc7d1dfa4d6324c1594fec964384
yasser momtaz <yasser.momtaz@outlook.com>
2023-06-13 19:36:33 +0300

    zy el fol

--- BL/Dtos/ApartmentDetails.cs
+++ BL/Dtos/ApartmentDetails.cs
@@ -9,24 +9,24 @@ namespace BL.Dtos
     public class ApartmentDetails
     {
         public int Id { get; set; }
-        public string Title { get; set; }
+        public string? Title { get; set; }
 
         public string? BrokerPhone { get; set; }
 
         public string? BrokerEmail { get; set; }
 
-        public int MaxPrice { get; set; }
+        public int? MaxPrice { get; set; }
 
-        public string Address { get; set; }
+        public string? Address { get; set; }
 
-        public string City { get; set; }
-        public int Area { get; set; }
-        public string Description { get; set; }
-        public string Type { get; set; }
+        public string? City { get; set; }
+        public int? Area { get; set; }
+        public string? Description { get; set; }
+        public string? Type { get; set; }
 
 
-        public DateTime AdDate { get; set; }
-        public int Bedrooms { get; set; }
-        public int Bathrooms { get; set; }
+        public DateTime? AdDate { get; set; }
+        public int? Bedrooms { get; set; }
+        public int? Bathrooms { get; set; }
     }
 }
--- BL/Dtos/ApartmentList.cs
+++ BL/Dtos/ApartmentList.cs
@@ -13,24 +13,24 @@ namespace BL.Dtos
     {
         [Key]
         public int Id { get; set; }
-        public string Title { get; set; }
+        public string? Title { get; set; }
 
         public string? BrokerPhone { get; set; }
 
         public string? BrokerEmail { get; set; }
 
-        public int MaxPrice { get; set; }
+        public int? MaxPrice { get; set; }
 
-        public string Address { get; set; }
+        public string? Address { get; set; }
 
-        public string City { get; set; }
-        public int Area { get; set; }
-        public string MiniDescription { get; set; }
+        public string? City { get; set; }
+        public int? Area { get; set; }
+        public string? MiniDescription { get; set; }
 
 
-        public DateTime AdDate { get; set; }
-        public int Bedrooms { get; set; }
-        public int Bathrooms { get; set; }
+        public DateTime? AdDate { get; set; }
+        public int? Bedrooms { get; set; }
+        public int? Bathrooms { get; set; }
 
     }
 }
