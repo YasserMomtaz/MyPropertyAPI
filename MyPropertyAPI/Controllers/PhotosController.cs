279cc0b5934f0a10332db200f2b90dfd2ba584af
mhmd_alaa <mhmd_alaa@yahoo.com>
2023-06-17 15:18:42 +0300

    zxzxz

--- MyPropertyAPI/Controllers/PhotosController.cs
+++ MyPropertyAPI/Controllers/PhotosController.cs
@@ -13,65 +13,67 @@ namespace MyPropertyAPI.Controllers
      
        
         [HttpPost]
-        public ActionResult<PhotoDto> Upload(IFormFile file )
+        public ActionResult<List<PhotoDto>> Upload(List<IFormFile> files)
         {
 
             #region Checking Extension
 
-            var extension = Path.GetExtension(file.FileName);
-
             //TODO: It's better to be part of appsettings.json
             var allowedExtenstions = new string[]
             {
-            ".png",
-            ".jpg",
-            ".svg"
+        ".png",
+        ".jpg",
+        ".svg"
             };
 
-            bool isExtensionAllowed = allowedExtenstions.Contains(extension,
-                StringComparer.InvariantCultureIgnoreCase);
-            if (!isExtensionAllowed)
+            var photoDtos = new List<PhotoDto>();
+
+            foreach (var file in files)
             {
-                return BadRequest(new PhotoDto(false, "Extension is not valid"));
+                var extension = Path.GetExtension(file.FileName);
+
+                bool isExtensionAllowed = allowedExtenstions.Contains(extension,
+                    StringComparer.InvariantCultureIgnoreCase);
+                if (!isExtensionAllowed)
+                {
+                    photoDtos.Add(new PhotoDto(false, "Extension is not valid"));
+                    continue;
+                }
+
+                #endregion
+
+
+                #region Checking Length
+
+                bool isSizeAllowed = file.Length is > 0 and <= 4_000_000;
+                if (!isSizeAllowed)
+                {
+                    photoDtos.Add(new PhotoDto(false, "Size is not allowed"));
+                    continue;
+                }
+
+                #endregion
+
+                #region Storing The Image
+
+                var newFileName = $"{Guid.NewGuid()}{extension}";
+                var imagesPath = Path.Combine(Environment.CurrentDirectory, "Photos");
+                var fullFilePath = Path.Combine(imagesPath, newFileName);
+
+                using var stream = new FileStream(fullFilePath, FileMode.Create);
+                file.CopyTo(stream);
+
+                #endregion
+
+                #region Generating URL
+
+                var url = $"{Request.Scheme}://{Request.Host}/Photos/{newFileName}";
+                photoDtos.Add(new PhotoDto(true, "Success", url));
+
+                #endregion
             }
 
-            #endregion
-
-
-            #region Checking Length
-
-            bool isSizeAllowed = file.Length is > 0 and <= 4_000_000;
-            //bool isSizeAllowed = file.Length > 0 && file.Length <= 4_000_000;
-            if (!isSizeAllowed)
-            {
-                return BadRequest((false, "Size is not allowed"));
-            }
-
-            #endregion
-
-            #region Storing The Image
-
-            var newFileName = $"{Guid.NewGuid()}{extension}";
-            var imagesPath = Path.Combine(Environment.CurrentDirectory, "Photos");
-            var fullFilePath = Path.Combine(imagesPath, newFileName);
-
-            using var stream = new FileStream(fullFilePath, FileMode.Create);
-            file.CopyTo(stream);
-
-            #endregion
-
-            #region Generating URL
-
-            var url = $"{Request.Scheme}://{Request.Host}/Photos/{newFileName}";
-
-
-            return new  PhotoDto(true, "Success", url);
-
-
-
-            #endregion
-
-
+            return photoDtos;
 
         }
     }
